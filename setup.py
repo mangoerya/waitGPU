297b9c2efb25ab2a48933708a7238319e73c16dd
Eric Wong <ericwong@cs.cmu.edu>
2018-05-22 14:36:38 -0400

    bump version 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='waitGPU',
-    version='0.0.2',
+    version='0.0.3',
     description="A small Python library that waits for GPU conditions to be satisfied, and then setting `CUDA_VISIBLE_DEVICES` to the qualifying GPU on multi-GPU systems.",
     author='Eric Wong',
     author_email='ericwong@cs.cmu.edu',
