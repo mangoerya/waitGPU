321f28baf9bc4bc818c210ad810d53c649347650
lberrada <leonardbj@hotmail.fr>
2018-05-22 11:05:34 +0100

    Support for multiple GPUs and max number of processes per device

--- test.py
+++ test.py
@@ -3,4 +3,6 @@ import waitGPU
 waitGPU.wait(available_memory=9000)
 waitGPU.wait(utilization=50)
 waitGPU.wait(memory_ratio=0.5)
+waitGPU.wait(nproc=0)
 waitGPU.wait(gpu_ids=[1,2], utilization=50)
+waitGPU.wait(gpu_ids=[1,2], utilization=50, ngpu=2)
